f4e205a50e93d92a7b206bbbfb8b06469a18fbd4
Taufiq Adjie Sanjaya <taufikaji350@gmail.com>
2023-10-07 19:34:26 +0700

    feat: return form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
 import "./App.css";
+import { Col, Divider, Row } from "antd";
+import BookList from "./components/BookList";
+import MemberList from "./components/MemberList";
+import BorrowForm from "./components/BorrowForm";
+import ReturnForm from "./components/ReturnForm";
+import TransactionList from "./components/TransactionList";
 
 const App: React.FC = () => {
   return (
     <>
+      <Row className="min-h-screen pb-20" gutter={[0, 100]}>
+        <Col xs={{ span: 20, offset: 1 }} lg={{ span: 16, offset: 4 }}>
+          <Divider>List Of Available Books</Divider>
+          <BookList></BookList>
+        </Col>
+        <Col xs={{ span: 20, offset: 1 }} lg={{ span: 16, offset: 4 }}>
+          <Divider>Member List</Divider>
+          <MemberList></MemberList>
+        </Col>
+        <Col xs={{ span: 20, offset: 1 }} lg={{ span: 16, offset: 4 }}>
+          <Divider>Transaction List</Divider>
+          <TransactionList></TransactionList>
+        </Col>
+        <Col xs={{ span: 20, offset: 2 }} lg={{ span: 11, offset: 1 }}>
+          <Divider>Borrow Form</Divider>
+          <BorrowForm></BorrowForm>
+        </Col>
+        <Col xs={{ span: 20, offset: 2 }} lg={{ span: 11, offset: 1 }}>
+          <Divider>Return Form</Divider>
+          <ReturnForm></ReturnForm>
+        </Col>
+      </Row>
     </>
   );
 };
